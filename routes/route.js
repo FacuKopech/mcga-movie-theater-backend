5165a111358460bee555ee155fc4a017a8dcf344
Facu <facukopech@gmail.com>
2024-07-07 11:01:06 -0300

    added the new routes for the user register and movie update endpoints

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const loginController = require("../controllers/loginController");
+const {login, register} = require("../controllers/usersController");
 const authenticateUser = require("../middleware/authenticateUser");
-const { addMovie, deleteMovie, getMovieGenres } = require("../controllers/moviesController");
+const { addMovie, deleteMovie, getMovieGenres, updateMovie } = require("../controllers/moviesController");
 const getMovieImages = require("../controllers/tmdbController");
 
 const router = express.Router();
 
-router.post("/login", loginController);
+router.post("/login", login);
+router.post("/register", register);
 router.get("/logout", (req, res) => {
   res.clearCookie("token", {
     domain: process.env.FRONT_END_URL,
@@ -20,5 +21,6 @@ router.get("/logout", (req, res) => {
 router.get("/get-genres", getMovieGenres);
 router.post("/add-movie", authenticateUser, addMovie);
 router.delete("/delete-movie",authenticateUser, deleteMovie);
+router.put("/update-movie",authenticateUser, updateMovie);
 router.post("/get-movie-posters", getMovieImages);
 module.exports = router;
