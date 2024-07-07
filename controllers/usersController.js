e4451c375849a876624a8da5b856c0316bb293be
Facu <facukopech@gmail.com>
2024-07-07 10:58:31 -0300

    changed login controller for users controller and added register functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,7 +4,7 @@ const env = require("dotenv");
 const { createSecretToken } = require("../tokenGeneration/generateToken");
 env.config();
 
-const loginController = async (req, res) => {
+const login = async (req, res) => {
   const { email, password } = req.body;
   if (!(email && password)) {
     return res.status(400).json({ message: "Both email & password are required" });
@@ -17,7 +17,7 @@ const loginController = async (req, res) => {
   const token = createSecretToken(user._id, user.name, user.username, user.email);
   res.cookie("token", token, {
     domain: process.env.FRONT_END_URL,
-    path: "/", 
+    path: "/",
     expires: new Date(Date.now() + 3600 * 1000),
     secure: true,
     httpOnly: false,
@@ -25,4 +25,33 @@ const loginController = async (req, res) => {
   });
   res.status(200).json({ token });
 };
-module.exports = loginController;
+
+const register = async (req, res) => {
+  const { name, username, email, password } = req.body;
+  if (!(name && username && email && password)) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const usersCollection = db.collection("user");
+  const existingUser = await usersCollection.findOne({ email });
+  
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already exists" });
+  }
+
+  const newUser = {
+    name,
+    username,
+    email,
+    password: hashedPassword,
+  };
+  const result = await usersCollection.insertOne(newUser);
+
+  if (!result.acknowledged) {
+    res.status(500).json({ message: 'Failed to register user' });
+  }
+
+  res.status(200).json({ message: 'User registered successfully' });
+};
+
+module.exports = { login, register };
