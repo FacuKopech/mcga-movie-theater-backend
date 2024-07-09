e7bbd0bdf0bbab122f97efc3bb08aa5ba98d98a1
Facu <facukopech@gmail.com>
2024-07-08 21:55:59 -0300

    modified CORS config

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const authRoute = require('./routes/route.js');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const port = process.env.PORT;
+app.use(cors({
+  origin: process.env.FRONT_END_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
