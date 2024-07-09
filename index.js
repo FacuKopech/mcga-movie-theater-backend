3cf3b383c3a97003e8ce5cb3d7a762eb0541b55c
Facu <facukopech@gmail.com>
2024-07-08 21:46:43 -0300

    modified CORS config

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use("/api", authRoute);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.BACK_END_URL);
+  res.header("Access-Control-Allow-Origin", process.env.FRONT_END_URL);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
