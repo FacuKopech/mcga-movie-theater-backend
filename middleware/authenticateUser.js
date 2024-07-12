1fa612b5ed0fb9cfa4892a7c0e8cb73a9e18854c
Facu <facukopech@gmail.com>
2024-07-12 09:04:17 -0300

    added some logs to middleware

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
+  console.log("authenticateUser middleware called");
   const token = req.cookies.token;
   if (!token) {
+    console.log("No token provided");
     return res.status(401).json({ message: "Access Denied" });
   }
 
