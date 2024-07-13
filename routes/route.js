2159f491d29c0ddd662b3f0910abc22aa7ac268d
Facu <facukopech@gmail.com>
2024-07-13 16:56:09 -0300

    deleted check auth endpoint

--- routes/route.js
+++ routes/route.js
@@ -22,8 +22,5 @@ router.post("/add-movie", authenticateUser, addMovie);
 router.delete("/delete-movie", authenticateUser, deleteMovie);
 router.put("/update-movie", authenticateUser, updateMovie);
 router.post("/get-movie-posters", getMovieImages);
-router.get("/check-auth", authenticateUser, (req, res) => {
-  res.status(200).json({ message: "Authenticated" });
-});
 
 module.exports = router;
