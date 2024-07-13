0b24a4da18c5b7fb3a05fd2382023377b90c9c55
Facu <facukopech@gmail.com>
2024-07-13 15:01:46 -0300

    added route to check authentication

--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {login, register} = require("../controllers/usersController");
+const { login, register } = require("../controllers/usersController");
 const authenticateUser = require("../middleware/authenticateUser");
 const { addMovie, deleteMovie, getMovieGenres, updateMovie } = require("../controllers/moviesController");
 const getMovieImages = require("../controllers/tmdbController");
@@ -19,7 +19,11 @@ router.get("/logout", (req, res) => {
 });
 router.get("/get-genres", getMovieGenres);
 router.post("/add-movie", authenticateUser, addMovie);
-router.delete("/delete-movie",authenticateUser, deleteMovie);
-router.put("/update-movie",authenticateUser, updateMovie);
+router.delete("/delete-movie", authenticateUser, deleteMovie);
+router.put("/update-movie", authenticateUser, updateMovie);
 router.post("/get-movie-posters", getMovieImages);
+router.get("/check-auth", authenticateUser, (req, res) => {
+  res.status(200).json({ message: "Authenticated" });
+});
+
 module.exports = router;
